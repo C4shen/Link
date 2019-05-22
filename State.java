270bce75f54ac71b73709aff2686965402357a86
Cashen <cashenadkins@googlemail.com>
2019-05-22 12:20:45 +0200

    Hinzufügen von Kommentaren bei der Klasse State

--- State.java
+++ State.java
@@ -1,9 +1,10 @@
 import java.awt.Graphics;
 /**
-  * @author Cashen Adkins,Janni Röbbecke, www.quizdroid.wordpress.com
-  * @version 0.02 (22.05.2019)
-  * @since 0.01 (22.05.2019
-  */
+ * 
+ * @author Cashen Adkins,Janni Röbbecke, www.quizdroid.wordpress.com
+ * @version 0.02 (22.05.2019)
+ * @since 0.01 (22.05.2019
+ */
 public abstract class State 
 {
     private static State currentState = null;
@@ -13,17 +14,39 @@ public abstract class State
       this.game = game;
     }
     
-        public static void setState(State state)
+    /**
+     * Ändert den State des Spiels
+     * @author Cashen Adkins, Janni Röbbecke, www.quizdroid.wordpress.com
+     * @param state Der neue State des Spiels.
+     * @since 0.01 (22.05.2019)
+     */
+    public static void setState(State state)
     {
       currentState = state;
     }
     
+    /**
+     * Gibt den State des Spiels wieder.
+     * @author Cashen Adkins, Janni Röbbecke, www.quizdroid.wordpress.com
+     * @return Momentaner State des Spiels.
+     * @since 0.01 (22.05.2019)
+     */
     public static State getState()
     {
       return currentState;
     }
     
+    /**
+     * Aktualisiert den State.
+     * @author Cashen Adkins, Janni Röbbecke, www.quizdroid.wordpress.com
+     * @since 0.01 (22.05.2019)
+     */
     public abstract void update();
     
+    /**
+     * Rendert den State.
+     * @author Cashen Adkins, Janni Röbbecke, www.quizdroid.wordpress.com
+     * @since 0.01 (22.05.2019)
+     */
     public abstract void render(Graphics g);
 }
