48c9c060059d4e43da35f2a87c6900b1cb70ac0c
Jakob Kleine <jakobkleine@gmx.de>
2019-05-23 15:50:42 +0200

    beste Sache hinzugefügt

--- IhrStinktLMAO.java
+++ IhrStinktLMAO.java
@@ -2,32 +2,9 @@
 /**
  * Beschreiben Sie hier die Klasse IhrStinktLMAO.
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * Beste Klasse vong Projekt her,weil meiste Quellcode yeet
  */
 public class IhrStinktLMAO
-{
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse IhrStinktLMAO
-     */
-    public IhrStinktLMAO()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+{ 
+    // Liebe Grüße: Team Hüpfburg
 }
